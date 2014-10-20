b05c3fdf181c97eea2f4681fd60a801b62311ed1
ej222pj <ej222pj@student.lnu.se>
2014-10-20 13:34:53 +0200

    Forrward

--- BildBlogg/view/EditProfileView.php
+++ BildBlogg/view/EditProfileView.php
@@ -4,9 +4,34 @@ namespace view;
 
 class EditProfileView{
 	
-	public function didUserPressSave(){
+	// public function didUserPressSave(){
+		// if(isset($_POST['save'])){
+			// return true;
+		// }
+	// }
+	
+		public function didUserPressSave(){
 		if(isset($_POST['save'])){
+			if(($_POST["newusername"]) == "" && ($_POST["oldpassword"]) == ""){
+				$this->RegUvalue = $_POST["regusername"];
+				$this->message = "Användarnamnet har för få tecken. Minst 3 tecken!\nLösenordet har för få tecken. Minst 6 tecken";
+			}
+			elseif(strlen(($_POST["newusername"])) < 3){
+				$this->RegUvalue = $_POST["newusername"];
+				$this->message = "Användarnamnet har för få tecken. Minst 3 tecken";
+			}
+			elseif(($_POST["oldpassword"]) == "" && ($_POST["newusername"]) != "" || strlen(($_POST["oldpassword"])) < 6) {
+				$this->RegUvalue = $_POST["newusername"];
+				$this->message = "Lösenordet har för få tecken. Minst 6 tecken";
+			}
+			elseif(($_POST["repnewpassword"]) !== ($_POST["newpassword"])) {
+				$this->RegUvalue = $_POST["newusername"];
+				$this->message = "Lösenorden matchar inte";
+			}
 			return true;
+		}
+		else{
+			return false;
 		}
 	}
 
